d05b8b4d8f60a22040b2b1933942980b9b0424eb
bentjh01 <bteh002@e.ntu.edu.sg>
2024-03-16 16:46:45 +0700

    reorganise

---
+++ init_scripts/init_ros_dep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# sudo apt-get update
+
+sudo apt install -y ros-melodic-interactive-markers \
+	ros-melodic-vision-opencv 	\
+	ros-melodic-cv-bridge           \
+	ros-melodic-image-transport     \
+	ros-melodic-stage-ros           \
+	ros-melodic-map-server          \
+	ros-melodic-laser-geometry      \
+	ros-melodic-tf                  \
+	ros-melodic-pcl-*               \
+	ros-melodic-libg2o              \
+	ros-melodic-rplidar-ros         \
+	ros-melodic-rviz                \
+	protobuf-compiler               \
+	libprotobuf-dev                 \
+	libsuitesparse-dev              \
+	libgoogle-glog-dev              
+
+	# ros-melodic-opencv3             \
